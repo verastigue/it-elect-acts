0902b91e2e640c7826cbab96f90242b5d4754745
uncledev <arce.jhonbrian@gmail.com>
2024-03-12 10:51:27 +0800

    calc

---
+++ Calculator/Act_3/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub btn_7_Click(sender As Object, e As EventArgs) Handles btn_7.Click
+        txt_panel.Text = 0
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
